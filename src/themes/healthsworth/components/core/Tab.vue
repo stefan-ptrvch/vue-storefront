61d392634956dbaf35c1cef1c9b0a22be1f498e0
Stefan X220 <petzimir@gmail.com>
2018-11-23 13:47:45 +0100

    Added healthsworth theme.

---
+++ src/themes/healthsworth/components/core/Tab.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-show="isActive"><slot/></div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      required: true,
+      type: String
+    },
+
+    selected: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+      isActive: false
+    }
+  },
+
+  computed: {
+    href () {
+      return '#' + this.name.toLowerCase().replace(/ /g, '-')
+    }
+  },
+
+  mounted () {
+    this.isActive = this.selected
+  }
+}
+</script>
